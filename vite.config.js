dec9af0100c425765accd3771cf87b7b1b789045
yogeshpusarla <yogesh.pusarla@gmail.com>
2025-09-20 23:13:53 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',   // custom domain serves from the root
+  base: '/', // custom domain serves at the root
 })
