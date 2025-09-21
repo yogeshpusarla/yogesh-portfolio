58fe6d63897ec9fadd5205080fe2d8e644d37aaa
yogeshpusarla <yogesh.pusarla@gmail.com>
2025-09-20 22:37:07 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 // If deploying to https://<user>.github.io or using Vercel/Netlify, leave as '/'
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/',    // custom domain serves from root
 })
