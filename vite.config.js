8d31291600e34266de09b7c3a0b7ae2878c0847b
yogeshpusarla <yogesh.pusarla@gmail.com>
2025-09-20 22:42:24 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // If deploying to https://<user>.github.io/REPO_NAME/ change base to '/REPO_NAME/'
 // If deploying to https://<user>.github.io or using Vercel/Netlify, leave as '/'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  base: '/',    // custom domain serves from root
+  base: '/', // custom domain = root path
 })
